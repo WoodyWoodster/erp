5b24ab6f3a95f45799f9c50637acd6091f863736
Andrew Woodward <11779108+WoodyWoodster@users.noreply.github.com>
2024-11-24 17:02:29 -0600

    Fixed incorrect terraform setup for bastion and ec2

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,6 +2,16 @@ output "jumpbox_public_ip" {
   value = aws_instance.jumpbox.public_ip
 }
 
+output "jumpbox_private_ip" {
+  value = aws_instance.jumpbox.vpc_security_group_ids
+}
+
 output "web_private_ip" {
   value = aws_instance.web.private_ip
+}
+
+resource "local_file" "private_key" {
+  content         = tls_private_key.bastion_ssh_key.private_key_pem
+  filename        = "gndwrk.pem"
+  file_permission = "0600"
 }
