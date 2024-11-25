7435027d95712cf7b9317c119e33a1311f7fc049
Andrew Woodward <11779108+WoodyWoodster@users.noreply.github.com>
2024-11-25 13:58:26 -0600

    Added aws_lb_listener and target_group_attachment

--- terraform/load_balancer.tf
+++ terraform/load_balancer.tf
@@ -22,3 +22,21 @@ resource "aws_lb_target_group" "web_tg" {
     timeout  = 5
   }
 }
+
+resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web_tg.arn
+  }
+}
+
+resource "aws_lb_target_group_attachment" "web_tg_attachment" {
+  target_group_arn = aws_lb_target_group.web_tg.arn
+  target_id        = aws_instance.web.id
+  port             = 80
+
+}
